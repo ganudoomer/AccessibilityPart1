8954550351fbc5daecb1209f5dc9af71d165edbc
ganudoomer <ganuiscool@gmail.com>
2021-12-27 21:16:16 +0530

    Initial Commit

---
+++ AccessibilityPart1/AccessibilityPart1App.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityPart1App.swift
+//  AccessibilityPart1
+//
+//  Created by Sree on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityPart1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
