3135bfa9552d6c062a7f0d85468d97883af09a13
ganudoomer <ganuiscool@gmail.com>
2021-12-27 21:46:09 +0530

    Add directional option for voice over

--- AccessibilityPart1/ContentView.swift
+++ AccessibilityPart1/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
     let pictures = [
     "ales-krivec-15949",
     "galina-n-189483",
@@ -23,11 +24,48 @@ struct ContentView: View {
     @State private var selectedPicture = Int.random(in: 0...3)
     
     var body: some View {
-        Image(pictures[selectedPicture]).resizable().scaledToFit().onTapGesture{
-            selectedPicture = Int.random(in: 0...3)
-        }.accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+        VStack {
+            Image(pictures[selectedPicture]).resizable().scaledToFit().onTapGesture{
+                selectedPicture = Int.random(in: 0...3)
+            }.accessibilityLabel(labels[selectedPicture])
+                .accessibilityAddTraits(.isButton)
+                .accessibilityRemoveTraits(.isImage)
+            // Hiding images in voice over
+            Image(decorative: "nicolas-tissot-335096").accessibilityHidden(true)
+            
+            VStack {
+                Text("Your score is ")
+                Text("1000").font(.title)
+            }.accessibilityElement(children: .combine)
+            // Or Hide .combine and Add Label
+            
+            VStack {
+                Text("Value:\(value)")
+                
+                Button("Increment"){
+                    value += 1
+                }
+                Button("Decrement"){
+                    value -= 1
+                }
+            }.accessibilityElement()
+                .accessibilityLabel("Value").accessibilityValue(String(value)).accessibilityAdjustableAction { direction in
+                    switch direction {
+                    case .increment :
+                         value += 1
+                    case .decrement :
+                        value -= 1
+                    default:
+                        print("Not Handled")
+                    }
+                    
+                }
+            
+            
+            
+        }
+  
+        
     }
 }
 
